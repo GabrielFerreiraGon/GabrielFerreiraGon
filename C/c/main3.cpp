2923f1493edaf58f40d520c2533c92f0c3937e2e
GabrielFerreiraGon <154000634+GabrielFerreiraGon@users.noreply.github.com>
2025-07-07 22:21:45 -0300

    All of my development files
    All of my development files

---
+++ C/c/main3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+
+int con = 0;
+int usuario = 0;
+int s = 0;
+
+
+
+while (con <=s )
+{
+	printf("Usuario :");
+	scahf("%s", &usuario);
+	con += 1 ;
+		
+}
+
+
