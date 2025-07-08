88653320117f14e5cebe6f5edc5f996614b7d1d1
GabrielFerreiraGon <154000634+GabrielFerreiraGon@users.noreply.github.com>
2025-07-07 22:19:26 -0300

    upload files
    All of my development files

---
+++ my project/js & php/home.php
@@ -0,0 +1,14 @@
+<?php
+        $menu = ['home' => ' my first page'];
+ ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> php </title>
+
+    </head>
+    <body>
+        <?php  echo $menu['home']; ?>
+    </body>
+</html>
